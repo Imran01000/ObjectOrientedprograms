b89f206b663154bb069cd2197f085b140593f724
Imran01000 <imshaik01000@gmail.com>
2020-03-14 12:10:37 +0530

    ADDED:Added validation for name ,full name,contactnumber,and date and replaced that all with my information.

--- RegularExpression.java
+++ RegularExpression.java
@@ -6,27 +6,37 @@ public class RegularExpression {
 
 	String patternString;
 	String targetString = "Hello Faisal, We have your full\n" + 
-			"name as Faisal Shaikh in our system. your contact number is 8754212512.\n" + 
+			"name as Faisal Khan in our system. your contact number is 8754212512 .\n" + 
 			"Please,let us know in case of any clarification Thank you BridgeLabz 01/01/2016.";
+	String[] stringToReplace = {"Faisal"," Khan"," 8754212512","01/01/2016"};
+	String[] stringWithreplace = {"Imran"," Shaikh"," 9654123531","14/02/2020"};
+	String[] validateString = {"^[A-Z]","[a-z]","[7-9][0-9]{9}","[\\d]{2}[/][\\d]{2}[/][\\d]{4}"};
+	int iteration;
+	
 	public void checkMatch()
 	{
-
-		Pattern pattern = Pattern.compile("^[0-9] {10}");
-		Matcher match = pattern.matcher(targetString);
-		while(true)
+		for(iteration = 0 ; iteration  < stringToReplace.length ; iteration++)
 		{
-			if(match.find())
+			Pattern pattern = Pattern.compile(validateString[iteration]);
+			Matcher match = pattern.matcher(targetString);
+			while(true)
 			{
-				System.out.println("Validate number!!!");
-				break;
+				
+				if(match.find())
+				{
+					System.out.println(match.start());
+					targetString = targetString.replace(stringToReplace[iteration], stringWithreplace[iteration]);
+					break;
+				}
+				else
+				{
+					System.out.println("Not present!!!!");
+					break;
+				}
 			}
-			else
-			{
-				System.out.println("Not validate number dear!!!!");
-				break;
-			}
+			System.out.println(targetString);
 		}
-
+		
 	}	
 	public static void main(String[] args) {
 		RegularExpression exp = new RegularExpression();
